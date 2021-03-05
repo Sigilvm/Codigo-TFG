7a19fb1841831450e4bd05f81f76a7eeb4fa037e
Sigilvm <63403529+Sigilvm@users.noreply.github.com>
2021-03-05 23:01:25 +0100

    Unificación con el resto de ficheros

--- testing.py
+++ testing.py
@@ -1,16 +1,15 @@
 from funciones import *
-####################from methods import *
+from methods import *
 import time
 import matplotlib.pyplot as plt
-from PSO16granularidad import *
-from DE12granularidad import *
-from FA12granularidad import *
-from ABC12granularidad import *
+from PSO_granularidad import *
+from DE_granularidad import *
+from FA_granularidad import *
+from ABC_granularidad import *
 
 def testVelocidades():
     funs = [fun2,fun3,fun8,funHimmel,funBooth]
     numEjs = 11
-
     for i in range(len(funs)):
         t = 0
         for j in range(numEjs):
@@ -29,7 +28,6 @@ def testVelocidades():
                 t += result
             #print("La ejecucion ", j, "-esima ha tardado ", result)
         print("El PSO (paralelizado) aplicado a la funcion numero ", i, " ha tardado de media ", t/(numEjs-1), " segundos")
-
     for i in range(len(funs)):
         t = 0
         for j in range(numEjs):
@@ -67,7 +65,7 @@ def testVelocidades():
                 t += result
             #print("La ejecucion ", j, "-esima ha tardado ", result)
         print("El DE (paralelizado) aplicado a la funcion numero ", i, " ha tardado de media ", t/(numEjs-1), " segundos")
-
+        
     for i in range(len(funs)):
         t = 0
         for j in range(numEjs):
@@ -90,57 +88,33 @@ def testVelocidades():
         print("El FA (paralelizado) aplicado a la funcion numero ", i, " ha tardado de media ", t/(numEjs-1), " segundos")
 
 
-def testGranularidadesPSO(g1,l1,g2,l2,g3,l3,fun_index,args):
-    funs = [fun3]
+def testGranularidadesPSO(reps,g1,l1,g2,l2,g3,l3,fun,args):
     quantity, it_number, inertia, indiv, social = args
-    o = PSO(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],VRange=funs[fun_index][2],
-            inertia=inertia, indiv=indiv, social=social, f=funs[fun_index][0], gran=g1, lapso=l1)
-    
-    a = PSO(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],VRange=funs[fun_index][2],
-            inertia=inertia, indiv=indiv, social=social, f=funs[fun_index][0], gran=g1, lapso=l1) #repetimos pa que los tiempos de la primera compilacion no importen
-    a_its = a[0][:,0]
-    a_tiempos = a[0][:,1]
-    print("tiempo total con la primera configuracion: ", a[2], ", best: ", a[1])
-    
-    b = PSO(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],VRange=funs[fun_index][2],
-            inertia=inertia, indiv=indiv, social=social, f=funs[fun_index][0], gran=g2, lapso=l2)
-    b_its = b[0][:,0]
-    b_tiempos = b[0][:,1]
-    print("tiempo total con la segunda configuracion: ", b[2], ", best: ", b[1])
-    
-    c = PSO(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],VRange=funs[fun_index][2],
-            inertia=inertia, indiv=indiv, social=social, f=funs[fun_index][0], gran=g3, lapso=l3)
-    c_its = c[0][:,0]
-    c_tiempos = c[0][:,1]
-    print("tiempo total con la tercera configuracion: ", c[2], ", best: ", c[1])
-    
-
-    plt.scatter(*zip(*a_its[:]),s=0.7)
-    plt.scatter(*zip(*b_its[:]),s=0.7)
-    plt.scatter(*zip(*c_its[:]),s=0.7)
-    #plt.show()
-    plt.scatter(*zip(*a_tiempos[:]),s=0.7)
-    plt.scatter(*zip(*b_tiempos[:]),s=0.7)
-    plt.scatter(*zip(*c_tiempos[:]),s=0.7)
-    #plt.show()
-    return (a,b,c)
-
-def media():
-    for i in range(15):
+    o = PSOGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],VRange=fun[2],
+            inertia=inertia, indiv=indiv, social=social, f=fun[0], gran=g1, lapso=l1) #primera llamada para descartar el tiempo de compilacion inicial
+    for i in range(reps):
+        a = PSOGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],VRange=fun[2],
+            inertia=inertia, indiv=indiv, social=social, f=fun[0], gran=g1, lapso=l1) 
+        a = a[0]
+        b = PSOGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],VRange=fun[2],
+            inertia=inertia, indiv=indiv, social=social, f=fun[0], gran=g2, lapso=l2) #repetimos pa que los tiempos de la primera compilacion no importen
+        b = b[0]
+        c = PSOGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],VRange=fun[2],
+            inertia=inertia, indiv=indiv, social=social, f=fun[0], gran=g3, lapso=l3) #repetimos pa que los tiempos de la primera compilacion no importen
+        c = c[0]
         if i ==0:
-            a,b,c=testGranularidadesPSO(1,1000,4,250,20,50,0,(100,1000,0.1,1.9,2.1))
+            p,q,r = a,b,c
         else:
-            k=testGranularidadesPSO(1,1000,4,250,20,50,0,(100,1000,0.1,1.9,2.1))
-            a += k[0]
-            b += k[1]
-            c += k[2]
-    a,b,c = (a/15,b/15,c/15)
-    a_its = a[0][:,0]
-    a_tiempos = a[0][:,1]
-    b_its = b[0][:,0]
-    b_tiempos = b[0][:,1]
-    c_its = c[0][:,0]
-    c_tiempos = c[0][:,1]
+            p += a
+            q += b
+            r += c
+    p,q,r = (p/reps,q/reps,r/reps)
+    a_its = p[:,0]
+    a_tiempos = p[:,1]
+    b_its = q[:,0]
+    b_tiempos = q[:,1]
+    c_its = r[:,0]
+    c_tiempos = r[:,1]
     
     plt.scatter(*zip(*a_its[:]),s=0.7)
     plt.scatter(*zip(*b_its[:]),s=0.7)
@@ -150,31 +124,34 @@ def media():
     plt.scatter(*zip(*b_tiempos[:]),s=0.7)
     plt.scatter(*zip(*c_tiempos[:]),s=0.7)
     plt.show()
-    
-    
-def testGranularidadesDE(g1,l1,g2,l2,g3,l3,fun_index,args):
-    funs = funTodas
+
+def testGranularidadesDE(reps,g1,l1,g2,l2,g3,l3,fun,args):
     quantity, it_number, CP, F = args
-    o = DE(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], CP=CP, F=F,
-           Range=funs[fun_index][2], f=funs[fun_index][0], gran=g1,lapso=l1)
-    
-    a = DE(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], CP=CP, F=F,
-           Range=funs[fun_index][2], f=funs[fun_index][0], gran=g1,lapso=l1) #repetimos pa que los tiempos de la primera compilacion no importen
-    a_its = a[0][:,0]
-    a_tiempos = a[0][:,1]
-    print("tiempo total con la primera configuracion: ", a[2], ", best: ", a[1])
-    
-    b = DE(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], CP=CP, F=F,
-           Range=funs[fun_index][2], f=funs[fun_index][0], gran=g2,lapso=l2)
-    b_its = b[0][:,0]
-    b_tiempos = b[0][:,1]
-    print("tiempo total con la segunda configuracion: ", b[2], ", best: ", b[1])
-    
-    c= DE(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], CP=CP, F=F,
-           Range=funs[fun_index][2], f=funs[fun_index][0], gran=g3,lapso=l3)
-    c_its = c[0][:,0]
-    c_tiempos = c[0][:,1]
-    print("tiempo total con la tercera configuracion: ", c[2], ", best: ", c[1])
+    o = DEGran(quantity=quantity, it_number=it_number, dim=fun[1], CP=CP, F=F,
+           Range=fun[2], f=fun[0], gran=g1,lapso=l1) #primera llamada para descartar el tiempo de compilacion inicial
+    for i in range(reps):
+        a = DEGran(quantity=quantity, it_number=it_number, dim=fun[1], CP=CP, F=F,
+               Range=fun[2], f=fun[0], gran=g1,lapso=l1)  
+        a = a[0]
+        b = DEGran(quantity=quantity, it_number=it_number, dim=fun[1], CP=CP, F=F,
+               Range=fun[2], f=fun[0], gran=g2,lapso=l2)  
+        b = b[0]
+        c = DEGran(quantity=quantity, it_number=it_number, dim=fun[1], CP=CP, F=F,
+               Range=fun[2], f=fun[0], gran=g3,lapso=l3)  
+        c = c[0]
+        if i ==0:
+            p,q,r = a,b,c
+        else:
+            p += a
+            q += b
+            r += c
+    p,q,r = (p/reps,q/reps,r/reps)
+    a_its = p[:,0]
+    a_tiempos = p[:,1]
+    b_its = q[:,0]
+    b_tiempos = q[:,1]
+    c_its = r[:,0]
+    c_tiempos = r[:,1]
     
     plt.scatter(*zip(*a_its[:]),s=0.7)
     plt.scatter(*zip(*b_its[:]),s=0.7)
@@ -184,30 +161,34 @@ def testGranularidadesDE(g1,l1,g2,l2,g3,l3,fun_index,args):
     plt.scatter(*zip(*b_tiempos[:]),s=0.7)
     plt.scatter(*zip(*c_tiempos[:]),s=0.7)
     plt.show()
-
-def testGranularidadesFA(g1,l1,g2,l2,g3,l3,fun_index,args):
-    funs = funTodas
+    
+def testGranularidadesFA(reps,g1,l1,g2,l2,g3,l3,fun,args):
     quantity, it_number, alfa0,reduct,gamma,beta0 = args
-    o = FA(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],
-           alfa0=alfa0,reduct=reduct,gamma=gamma,beta0=beta0, f=funs[fun_index][0], gran=g1,lapso=l1)
-    
-    a = FA(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],
-           alfa0=alfa0,reduct=reduct,gamma=gamma,beta0=beta0, f=funs[fun_index][0], gran=g1,lapso=l1) #repetimos pa que los tiempos de la primera compilacion no importen
-    a_its = a[0][:,0]
-    a_tiempos = a[0][:,1]
-    print("tiempo total con la primera configuracion: ", a[2], ", best: ", a[1])
-    
-    b = FA(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],
-           alfa0=alfa0,reduct=reduct,gamma=gamma,beta0=beta0, f=funs[fun_index][0], gran=g2,lapso=l2)
-    b_its = b[0][:,0]
-    b_tiempos = b[0][:,1]
-    print("tiempo total con la segunda configuracion: ", b[2], ", best: ", b[1])
-
-    c = FA(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],
-           alfa0=alfa0,reduct=reduct,gamma=gamma,beta0=beta0, f=funs[fun_index][0], gran=g3,lapso=l3)
-    c_its = c[0][:,0]
-    c_tiempos = c[0][:,1]
-    print("tiempo total con la tercera configuracion: ", c[2], ", best: ", c[1])
+    o = FAGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],
+           alfa0=alfa0,reduct=reduct,gamma=gamma,beta0=beta0, f=fun[0], gran=g1,lapso=l1)
+    for i in range(reps):
+        a = FAGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],
+               alfa0=alfa0,reduct=reduct,gamma=gamma,beta0=beta0, f=fun[0], gran=g1,lapso=l1)
+        a = a[0]
+        b = FAGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],
+               alfa0=alfa0,reduct=reduct,gamma=gamma,beta0=beta0, f=fun[0], gran=g2,lapso=l2)
+        b = b[0]
+        c = FAGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],
+               alfa0=alfa0,reduct=reduct,gamma=gamma,beta0=beta0, f=fun[0], gran=g3,lapso=l3)
+        c = c[0]
+        if i ==0:
+            p,q,r = a,b,c
+        else:
+            p += a
+            q += b
+            r += c
+    p,q,r = (p/reps,q/reps,r/reps)
+    a_its = p[:,0]
+    a_tiempos = p[:,1]
+    b_its = q[:,0]
+    b_tiempos = q[:,1]
+    c_its = r[:,0]
+    c_tiempos = r[:,1]
     
     plt.scatter(*zip(*a_its[:]),s=0.7)
     plt.scatter(*zip(*b_its[:]),s=0.7)
@@ -218,31 +199,33 @@ def testGranularidadesFA(g1,l1,g2,l2,g3,l3,fun_index,args):
     plt.scatter(*zip(*c_tiempos[:]),s=0.7)
     plt.show()
 
-
-def testGranularidadesABC(g1,l1,g2,l2,g3,l3,fun_index,args):
-    funs = funTodas
+def testGranularidadesABC(reps,g1,l1,g2,l2,g3,l3,fun,args):
     quantity, it_number, limit = args
-    limit = quantity*funs[fun_index][1]/2
-    o = ABC(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],
-           limit=limit, f=funs[fun_index][0], gran=g1,lapso=l1)
-    
-    a = ABC(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],
-           limit=limit, f=funs[fun_index][0], gran=g1,lapso=l1)
-    a_its = a[0][:,0]
-    a_tiempos = a[0][:,1]
-    print("tiempo total con la primera configuracion: ", a[2], ", best: ", a[1])
-    
-    b = ABC(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],
-           limit=limit, f=funs[fun_index][0], gran=g2,lapso=l2)
-    b_its = b[0][:,0]
-    b_tiempos = b[0][:,1]
-    print("tiempo total con la segunda configuracion: ", b[2], ", best: ", b[1])
-
-    c = ABC(quantity=quantity, it_number=it_number, dim=funs[fun_index][1], Range=funs[fun_index][2],
-           limit=limit, f=funs[fun_index][0], gran=g3,lapso=l3)
-    c_its = c[0][:,0]
-    c_tiempos = c[0][:,1]
-    print("tiempo total con la tercera configuracion: ", c[2], ", best: ", c[1])
+    o = ABCGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],
+           limit=limit, f=fun[0], gran=g1,lapso=l1)
+    for i in range(reps):
+        a = ABCGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],
+               limit=limit, f=fun[0], gran=g1,lapso=l1)
+        a = a[0]
+        b = ABCGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],
+               limit=limit, f=fun[0], gran=g2,lapso=l2)
+        b = b[0]
+        c = ABCGran(quantity=quantity, it_number=it_number, dim=fun[1], Range=fun[2],
+               limit=limit, f=fun[0], gran=g3,lapso=l3)
+        c = c[0]
+        if i ==0:
+            p,q,r = a,b,c
+        else:
+            p += a
+            q += b
+            r += c
+    p,q,r = (p/reps,q/reps,r/reps)
+    a_its = p[:,0]
+    a_tiempos = p[:,1]
+    b_its = q[:,0]
+    b_tiempos = q[:,1]
+    c_its = r[:,0]
+    c_tiempos = r[:,1]
     
     plt.scatter(*zip(*a_its[:]),s=0.7)
     plt.scatter(*zip(*b_its[:]),s=0.7)
@@ -252,6 +235,9 @@ def testGranularidadesABC(g1,l1,g2,l2,g3,l3,fun_index,args):
     plt.scatter(*zip(*b_tiempos[:]),s=0.7)
     plt.scatter(*zip(*c_tiempos[:]),s=0.7)
     plt.show()
+
+
+
 
 
 
